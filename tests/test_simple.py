d22b8f747a338f1d19a1af856b1c157573445d47
Srinivas M Kotamraju <89226662+srinivaskotamraju94@users.noreply.github.com>
2022-07-08 16:27:01 -0700

    Update test_simple.py

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -9,19 +9,20 @@ os.environ["CUDA_VISIBLE_DEVICES"] = ""
 def test_inference():
     model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32-quickgelu', pretrained='laion400m_e32')
     checkpoint = torch.load("/rapids/notebooks/open_clip/src/logs/OpenAIFlickr8kTraining5/checkpoints/epoch_30.pt")
-    model = model.load_state_dict(checkpoint['state_dict'])
+    print(checkpoint['state_dict'])
+    #model = model.load_state_dict(checkpoint['state_dict'])
 
-    current_dir = os.path.dirname(os.path.realpath(__file__))
+    #current_dir = os.path.dirname(os.path.realpath(__file__))
 
-    image = preprocess(Image.open("/rapids/notebooks/host/ImagesOpenClipTrain/424379231_23f1ade134.jpg")).unsqueeze(0)
-    text = tokenizer.tokenize(["a diagram", "a dog", "a cat"])
+    #image = preprocess(Image.open("/rapids/notebooks/host/ImagesOpenClipTrain/424379231_23f1ade134.jpg")).unsqueeze(0)
+    #text = tokenizer.tokenize(["a diagram", "a dog", "a cat"])
 
-    with torch.no_grad():
-        image_features = model.encode_image(image)
-        text_features = model.encode_text(text)
+    #with torch.no_grad():
+        #image_features = model.encode_image(image)
+        #text_features = model.encode_text(text)
 
-        text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
-        return(text_probs)
+        #text_probs = (100.0 * image_features @ text_features.T).softmax(dim=-1)
+        #return(text_probs)
 
     #assert text_probs.cpu().numpy()[0].tolist() == [1.0, 0.0, 0.0]
     
