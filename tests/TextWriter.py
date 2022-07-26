e4076db62b0be3d39dd5ae2ef9da565b4b547bc6
Srinivas M Kotamraju <89226662+srinivaskotamraju94@users.noreply.github.com>
2022-07-26 12:33:09 -0700

    Update TextWriter.py

--- tests/TextWriter.py
+++ tests/TextWriter.py
@@ -11,66 +11,18 @@ def gettextfiles(filepath) :
     
     AllItemList = []
     
-    for jsonfiles in os.listdir(filepath) : 
-        
-        InputFile = [json.loads(line) for line in open(os.path.join(filepath,jsonfiles),'r')]
-        
-        # Pre-Processing 01 - Making Sure all the Item IDs have a Product Name and Asset 
-        
-        ItemId = []
-        ProductName = []
-        Asset = []
-        Asset_url = []
-
-        for i in range(len(InputFile)) :
-            InputFile[i].setdefault('product_name','NA')
-            InputFile[i].setdefault('assets','NA')
+    for val in os.listdir(filepath) :
+        if val != '.DS_Store' :
+            InputFile = [json.loads(line) for line in open(os.path.join(filepath,val),'r')]
             
-            if (InputFile[i]['product_name'] != 'NA' and InputFile[i]['assets'] != 'NA') : 
-                ItemId.append(InputFile[i]['itemid'])
-                ProductName.append(InputFile[i]['product_name'])
-                Asset.append(InputFile[i]['assets'])
-
-
-        
-        for i in range(len(Asset)) : 
-            assets_list = json.loads(Asset[i])
-            properties_list = []
-            for k in range(len(assets_list)) : 
-                for key in assets_list[k] : 
-                    if key == 'properties' :
-                        properties_list.append(assets_list[k][key])
-                    
-            for j in range(len(properties_list)) : 
-                for key in properties_list[j] : 
-                    if key == 'assetType' : 
-                        if properties_list[j][key] == 'PRIMARY':
-                            Asset_url.append(properties_list[j]['assetUrl'])
-                    
-
-
-
-        for item in range(len(ItemId)) :
-            temp_list = [] 
-            assets_list = json.loads(Asset[item])
-            properties_list = []
-            
-            for k in range(len(assets_list)) : 
-                for key in assets_list[k] : 
-                    if key == 'properties' : 
-                        properties_list.append(assets_list[k][key])
+            for value in range(len(InputFile)) :
+                TempList = []
+                TempList.append(InputFile[value]['item_id'])
+                TempList.append(InputFile[value]['product_name'])
+                TempList.append(InputFile[value]['asset_url'])
+                AllItemList.append(TempList)
     
-            for j in range(len(properties_list)) : 
-            
-                for val in properties_list[j] : 
-                    if val == 'assetType' : 
-                        if properties_list[j][val] == 'PRIMARY' : 
-                            temp_list.append(ItemId[item])
-                            temp_list.append(ProductName[item])
-                            temp_list.append(properties_list[j]['assetUrl'])
-                            AllItemList.append(temp_list)
-                            
-                            
+   
     return AllItemList
                     
 
